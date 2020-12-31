f95f1c3051f95b6e935f53386d17b9de7f481713
adarsh m <meh.adarsh.m@gmail.com>
2020-12-31 19:32:19 +0530

    mern stack

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/db',{useNewUrlParser:true,useUnifiedTopology:true},
     err => {
         if (!err)
-            console.log('Mongodb connection succeeded.')
+            console.log('Mongodb connection success.')
         else
             console.log('Error while connecting MongoDB : ' + JSON.stringify(err, undefined, 2))
     })
--- server/index.js
+++ server/index.js
@@ -1,22 +1,21 @@
-require('./db')
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require("./db");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-var employees = require('./controllers/employees')
-var assets = require('./controllers/assets')
-var assetsStatus = require('./controllers/assetsStatus')
-var user = require('./controllers/user')
+var employees = require("./controllers/employees");
+var assets = require("./controllers/assets");
+var assetsStatus = require("./controllers/assetsStatus");
+var user = require("./controllers/user");
 
+var app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Server started at ${PORT}`));
 
-var app = express()
-app.use(bodyParser.json())
-app.use(cors({origin:'http://localhost:3000'}))
-app.listen(4000,()=>console.log('Server started at : 4000'))
-
-
-app.use('/employees',employees)
-app.use('/assets',assets)
-app.use('/assetsStatus',assetsStatus)
-app.use('/user',user)
+app.use("/employees", employees);
+app.use("/assets", assets);
+app.use("/assetsStatus", assetsStatus);
+app.use("/user", user);
